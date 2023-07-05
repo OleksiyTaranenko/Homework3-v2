74333041ce4bcaf956280b5ec62d66c3b00a925f
OleksiyTaranenko <77008282+OleksiyTaranenko@users.noreply.github.com>
2023-07-05 14:57:29 +0300

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,27 +1,30 @@
-//And please uncomment this page :)
+let temp = "celsius";
+let F = 86;
+let C = 30;
+let K = 303.15;
 
-
-// let temp = 'fahrenheit';
-// let F = 86;
-// let C = 30;
-// let K = 303.15;
-
-// switch (temp) {
-//     case 'fahrenheit':
-//         console.log(F + '°F');
-//         console.log(K = C + 273.15 + '°K');
-//         console.log(C = (F - 32) * 5/9 + '°C');        
-//         break;
-//     case 'celsius':
-//         console.log(C + '°C');
-//         console.log(F = (C * 9/5) + 32 + '°F');
-//         console.log(K = C + 273.15 + '°K');
-//         break;
-//     case 'kelvin':
-//         console.log(K + '°K');
-//         console.log(C = K - 273.15 + '°C');
-//         console.log(F = (C * 9/5) + 32 + '°F');
-//         break;
-//     default:
-//         console.log('');
-// }
+switch (temp) {
+    case "fahrenheit":
+        K = C + 273.15;
+        C = ((F - 32) * 5) / 9;
+        console.log(F, "°F");
+        console.log(K, "°K");
+        console.log(C, "°C");
+        break;
+    case "celsius":
+        F = (C * 9) / 5 + 32;
+        K = C + 273.15;
+        console.log(C, "°C");
+        console.log(F, "°F");
+        console.log(K, "°K");
+        break;
+    case "kelvin":
+        C = K - 273.15;
+        F = (C * 9) / 5 + 32;
+        console.log(K, "°K");
+        console.log(C, "°C");
+        console.log(F, "°F");
+        break;
+    default:
+        console.log("");
+}
